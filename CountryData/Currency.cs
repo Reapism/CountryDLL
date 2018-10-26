89bb20cb0497c83149aad321571a02ac998262f5
REAPIX\Reapism <reapsprgm@gmail.com>
2018-10-26 18:51:59 -0400

    + ensured the get; on string properties dont return null. this happens when you deserialize a currency list that contained null in it.

--- CountryData/Currency.cs
+++ CountryData/Currency.cs
@@ -1,9 +1,7 @@
-﻿/// Country Data Class Library: 
-/// The Currency Class.
-///
-/// Author: Anthony Jaghab (c), all rights reserved.
-
+﻿using System;
+using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace CountryData {
 
@@ -12,6 +10,7 @@ namespace CountryData {
     /// <summary>
     /// A class containing information about the
     /// code, name, and symbol of a particular currency.
+    /// <para>Author: Anthony Jaghab (c), all rights reserved.</para>
     /// </summary>
 
     [DataContract]
@@ -33,7 +32,7 @@ namespace CountryData {
 
         [DataMember(Name = "code")]
         public string Code {
-            get => this.code;
+            get { if (this.code == null) return ""; else return this.code; }
             set {
                 if (value != null) { this.code = value; } // if value is null, don't set!
             }
@@ -45,7 +44,7 @@ namespace CountryData {
 
         [DataMember(Name = "name")]
         public string Name {
-            get => this.name;
+            get { if (this.name == null) return ""; else return this.name; }
             set {
                 if (value != null) { this.name = value; } // if value is null, don't set!
             }
@@ -57,7 +56,7 @@ namespace CountryData {
 
         [DataMember(Name = "symbol")]
         public string Symbol {
-            get => this.symbol;
+            get { if (this.symbol == null) return ""; else return this.symbol; }
             set {
                 if (value != null) { this.symbol = value; } // if value is null, don't set!
             }
@@ -73,9 +72,153 @@ namespace CountryData {
         /// </summary>
 
         public Currency() {
-            this.code = "USD";
-            this.name = "United States dollar";
-            this.symbol = "$";
+            this.code = "N/A";
+            this.name = "N/A";
+            this.symbol = "N/A";
+        }
+
+        /// <summary>
+        /// Attempts to serialize a Currency type. 
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, and returns false</para>
+        /// </summary>
+        /// <param name="path">Path to serialized object.</param>
+        /// <param name="type">Type to Serialize</param>
+
+        public bool SerializeJSON(string path) {
+            StreamWriter sw;
+            MemoryStream ms;
+            DataContractJsonSerializer ser;
+            byte[] data;
+            string utf8String;
+
+            try {
+                ser = new DataContractJsonSerializer(typeof(Currency));
+                ms = new MemoryStream();
+                ser.WriteObject(ms, this);
+                data = ms.ToArray();
+                utf8String = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+                sw = new StreamWriter(path, false, System.Text.Encoding.UTF8);
+                sw.Write(utf8String);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                return false;
+            }
+
+            ms.Close();
+            sw.Close();
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to serialize a Currency type. 
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, and returns false</para>
+        /// </summary>
+        /// <param name="path">Path to serialized object.</param>
+        /// <param name="type">Type to Serialize</param>
+
+        public bool SerializeXML(string path) {
+            StreamWriter sw;
+            MemoryStream ms;
+            DataContractSerializer ser;
+            byte[] data;
+            string utf8String;
+
+            try {
+                ser = new DataContractSerializer(typeof(Currency));
+                ms = new MemoryStream();
+                ser.WriteObject(ms, this);
+                data = ms.ToArray();
+                utf8String = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+                sw = new StreamWriter(path, false, System.Text.Encoding.UTF8);
+                sw.Write(utf8String);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                return false;
+            }
+
+            ms.Close();
+            sw.Close();
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a serialized Currency type and passes it out.
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, returns false, and
+        /// type = null</para>
+        /// </summary>
+        /// <param name="path">Path to deserialize object.</param>
+        /// <param name="type">Sets the calling argument to type.</param>
+
+        public static bool DeserializeJSON(string path, out Currency type) {
+            FileStream reader;
+            StreamReader sr;
+            byte[] bArr;
+            MemoryStream ms;
+            string utf8Str;
+
+            try {
+                reader = new FileStream(path, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(reader, System.Text.Encoding.UTF8);
+                utf8Str = sr.ReadToEnd();
+                bArr = System.Text.Encoding.Unicode.GetBytes(utf8Str);
+                ms = new MemoryStream(bArr);
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Currency));
+                type = (Currency)ser.ReadObject(ms);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                type = null;
+                return false;
+            }
+
+            reader.Close();
+            sr.Close();
+            ms.Close();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a serialized Language type and passes it out.
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, returns false, and
+        /// type = null</para>
+        /// </summary>
+        /// <param name="path">Path to deserialize object.</param>
+        /// <param name="type">Sets the calling argument to type.</param>
+
+        public static bool DeserializeXML(string path, out Currency type) {
+            FileStream reader;
+            StreamReader sr;
+            byte[] bArr;
+            MemoryStream ms;
+            string utf8Str;
+
+            try {
+                reader = new FileStream(path, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(reader, System.Text.Encoding.UTF8);
+                utf8Str = sr.ReadToEnd();
+                bArr = System.Text.Encoding.Unicode.GetBytes(utf8Str);
+                ms = new MemoryStream(bArr);
+                DataContractSerializer ser = new DataContractSerializer(typeof(Currency));
+                type = (Currency)ser.ReadObject(ms);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                type = null;
+                return false;
+            }
+
+            reader.Close();
+            sr.Close();
+            ms.Close();
+
+            return true;
         }
 
         /// <summary>
@@ -86,7 +229,7 @@ namespace CountryData {
         /// current values.</returns>
 
         public override string ToString() =>
-            $"The {this.name} ({this.symbol}) code is {this.code}";
+            $"Currency name: {this.name}\nSymbol: {this.symbol}\nCode:{this.code}\n";
 
     }
     #endregion
