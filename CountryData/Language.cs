17ddb70f41232ecd80978e6010910ec03820d229
REAPIX\Reapism <reapsprgm@gmail.com>
2018-10-26 18:53:04 -0400

    + ensured utf8 serialization/deserialization for both json and xml
    + ensured that get; properties of type string dont return NULL. this happens when you deserialize a language that stores null already.

--- CountryData/Language.cs
+++ CountryData/Language.cs
@@ -1,9 +1,7 @@
-﻿/// Country Data Class Library: 
-/// The Language Class.
-///
-/// Author: Anthony Jaghab (c), all rights reserved.
-
+﻿using System;
+using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace CountryData {
 
@@ -11,7 +9,9 @@ namespace CountryData {
 
     /// <summary>
     /// A class containing information about a language.
+    /// <para>Author: Anthony Jaghab (c), all rights reserved.</para>
     /// </summary>
+
     [DataContract]
     public class Language {
 
@@ -30,18 +30,19 @@ namespace CountryData {
         /// Property for the name of this language instance.
         /// </summary>
 
-        [DataMember(Name="name")]
+        [DataMember(Name = "name")]
         public string Name {
-            get => this.name;
+            get { if (this.name == null) return ""; else return this.name; }
             set { if (value != null) this.name = value; }
         }
 
         /// <summary>
         /// Property for the native name of this language instance.
         /// </summary>
+
         [DataMember(Name = "nativeName")]
         public string NativeName {
-            get => this.nativeName;
+            get { if (this.nativeName == null) return ""; else return this.nativeName; }
             set { if (value != null) this.nativeName = value; }
         }
 
@@ -50,11 +51,10 @@ namespace CountryData {
         /// A language code from the  International Organization 
         /// for Standardization to classifylanguages.
         /// </summary>
-        
 
         [DataMember(Name = "iso639_1")]
         public string ISO639_1 {
-            get => this.iso639_1;
+            get { if (this.iso639_1 == null) return ""; else return this.iso639_1; }
             set { if (value != null) this.iso639_1 = value; }
         }
 
@@ -66,7 +66,7 @@ namespace CountryData {
 
         [DataMember(Name = "iso639_2")]
         public string ISO639_2 {
-            get => this.iso639_2;
+            get { if (this.iso639_2 == null) return ""; else return this.iso639_2; }
             set { if (value != null) this.iso639_2 = value; }
         }
 
@@ -79,10 +79,154 @@ namespace CountryData {
         /// </summary>
 
         public Language() {
-            this.name = "English";
-            this.nativeName = "English";
-            this.iso639_1 = "en";
-            this.iso639_2 = "eng";
+            this.name = "N/A";
+            this.nativeName = "N/A";
+            this.iso639_1 = "N/A";
+            this.iso639_2 = "N/A";
+        }
+
+        /// <summary>
+        /// Attempts to serialize a Language type. 
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, and returns false</para>
+        /// </summary>
+        /// <param name="path">Path to serialized object.</param>
+        /// <param name="type">Type to Serialize</param>
+
+        public bool SerializeJSON(string path) {
+            StreamWriter sw;
+            MemoryStream ms;
+            DataContractJsonSerializer ser;
+            byte[] data;
+            string utf8String;
+
+            try {
+                ser = new DataContractJsonSerializer(typeof(Language));
+                ms = new MemoryStream();
+                ser.WriteObject(ms, this);
+                data = ms.ToArray();
+                utf8String = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+                sw = new StreamWriter(path, false, System.Text.Encoding.UTF8);
+                sw.Write(utf8String);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                return false;
+            }
+
+            ms.Close();
+            sw.Close();
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to serialize a Language type. 
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, and returns false</para>
+        /// </summary>
+        /// <param name="path">Path to serialized object.</param>
+        /// <param name="type">Type to Serialize</param>
+
+        public bool SerializeXML(string path) {
+            StreamWriter sw;
+            MemoryStream ms;
+            DataContractSerializer ser;
+            byte[] data;
+            string utf8String;
+
+            try {
+                ser = new DataContractSerializer(typeof(Language));
+                ms = new MemoryStream();
+                ser.WriteObject(ms, this);
+                data = ms.ToArray();
+                utf8String = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+                sw = new StreamWriter(path, false, System.Text.Encoding.UTF8);
+                sw.Write(utf8String);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                return false;
+            }
+
+            ms.Close();
+            sw.Close();
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a serialized Language type and passes it out.
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, returns false, and
+        /// type = null</para>
+        /// </summary>
+        /// <param name="path">Path to deserialize object.</param>
+        /// <param name="type">Sets the calling argument to type.</param>
+
+        public static bool DeserializeJSON(string path, out Language type) {
+            FileStream reader;
+            StreamReader sr;
+            byte[] bArr;
+            MemoryStream ms;
+            string utf8Str;
+
+            try {
+                reader = new FileStream(path, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(reader, System.Text.Encoding.UTF8);
+                utf8Str = sr.ReadToEnd();
+                bArr = System.Text.Encoding.UTF8.GetBytes(utf8Str);
+                ms = new MemoryStream(bArr);
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Language));
+                type = (Language)ser.ReadObject(ms);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                type = null;
+                return false;
+            }
+
+            reader.Close();
+            sr.Close();
+            ms.Close();
+
+            return true;
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a serialized Language type and passes it out.
+        /// 
+        /// <para>Internally catches an Exception, in that case,
+        /// writes to console the exception, returns false, and
+        /// type = null</para>
+        /// </summary>
+        /// <param name="path">Path to deserialize object.</param>
+        /// <param name="type">Sets the calling argument to type.</param>
+
+        public static bool DeserializeXML(string path, out Language type) {
+            FileStream reader;
+            StreamReader sr;
+            byte[] bArr;
+            MemoryStream ms;
+            string utf8Str;
+
+            try {
+                reader = new FileStream(path, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(reader, System.Text.Encoding.UTF8);
+                utf8Str = sr.ReadToEnd();
+                bArr = System.Text.Encoding.Unicode.GetBytes(utf8Str);
+                ms = new MemoryStream(bArr);
+                DataContractSerializer ser = new DataContractSerializer(typeof(Language));
+                type = (Language)ser.ReadObject(ms);
+            } catch (Exception e) {
+                Console.WriteLine("\nException found:" + e.ToString());
+                type = null;
+                return false;
+            }
+
+            reader.Close();
+            sr.Close();
+            ms.Close();
+
+            return true;
         }
 
         /// <summary>
@@ -92,10 +236,13 @@ namespace CountryData {
         /// <returns>Information on all the member variables
         /// current values.</returns>
 
-        public override string ToString() => $"The name of this languge is {this.name}" +
-            $" with a native name \"{this.nativeName}\". International Organization for " +
-            $"Standardization codes are iso639-1: {this.iso639_1} and iso639-2: {this.iso639_2}";
+        public override string ToString() => $"Language name: {this.name}\n" +
+            $"Native name: {this.nativeName}\niso639-1: " +
+            $"{this.iso639_1}\n iso639-2: {this.iso639_2}" +
+            $"\n";
+
+        #endregion
     }
-    #endregion
+
     #endregion
 }
